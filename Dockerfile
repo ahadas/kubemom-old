f0cd9398db2222bd747c2a804eb9606b26246581
Arik Hadas <arik.hadas1@gmail.com>
2018-12-04 00:33:44 +0200

    retrieve metrics

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3
 ADD script.py /
 ADD .kubeconfig /root/.kube/config
 RUN pip install kubernetes
+RUN pip install prometheus_client requests
 CMD [ "python", "./script.py" ]
--- script.py
+++ script.py
@@ -1,11 +1,21 @@
 from kubernetes import client, config
+from prometheus_client.parser import text_string_to_metric_families
+import requests
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+#v1 = client.CoreV1Api()
+#print("Listing pods with their IPs:")
+#ret = v1.list_pod_for_all_namespaces(watch=False)
+#for i in ret.items:
+#    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+print ('connecting...')
+metrics = requests.get("http://127.0.0.1:9101/metrics").content
+
+print ('connected')
+for family in text_string_to_metric_families(metrics):
+  for sample in family.samples:
+    print("Name: {0} Labels: {1} Value: {2}".format(*sample))
 
