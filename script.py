ab2e4ef949f25407550e059b11e9f084114bf8d6
Arik Hadas <arik.hadas1@gmail.com>
2018-12-30 19:59:41 +0000

    change the script to querying pods

--- script.py
+++ script.py
@@ -1,21 +1,22 @@
 from kubernetes import client, config
-from prometheus_client.parser import text_string_to_metric_families
-import requests
+#from prometheus_client.parser import text_string_to_metric_families
+#import requests
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 
-#v1 = client.CoreV1Api()
-#print("Listing pods with their IPs:")
-#ret = v1.list_pod_for_all_namespaces(watch=False)
-#for i in ret.items:
-#    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+print ('querying..')
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
-print ('connecting...')
-metrics = requests.get("http://127.0.0.1:9101/metrics").text
+#print ('connecting...')
+#metrics = requests.get("http://127.0.0.1:9101/metrics").text
 
-print ('connected')
-for family in text_string_to_metric_families(metrics):
-  for sample in family.samples:
-    print("Name: {0} Labels: {1} Value: {2}".format(*sample))
+#print ('connected')
+#for family in text_string_to_metric_families(metrics):
+#  for sample in family.samples:
+#    print("Name: {0} Labels: {1} Value: {2}".format(*sample))
 
