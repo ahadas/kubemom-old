c65639a39a1406fc44ed0b40db229f6f1c225d5f
Arik Hadas <arik.hadas1@gmail.com>
2019-01-03 11:22:41 +0200

    script

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ v1 = client.CoreV1Api()
 print("Listing pods and their respective VMIs:")
 ret = v1.list_namespaced_pod('default', watch=False, label_selector='kubevirt.io=virt-launcher', field_selector='spec.nodeName=node02')
 for i in ret.items:
-    print("%s\t-> %s" % (i.metadata.name, get_vmi(i.metadata.owner_references).name))
+    print("%s\t-> %s" % (i.metadata.name, get_vmi(i.metadata.owner_references).uid))
 
 #print ('connecting...')
 #metrics = requests.get("http://127.0.0.1:9101/metrics").text
@@ -25,4 +25,3 @@ for i in ret.items:
 #for family in text_string_to_metric_families(metrics):
 #  for sample in family.samples:
 #    print("Name: {0} Labels: {1} Value: {2}".format(*sample))
-
