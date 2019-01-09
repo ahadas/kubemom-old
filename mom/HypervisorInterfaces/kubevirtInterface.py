f1d01a56c9afac0cf45a297fb1d784afe05d3557
Arik Hadas <arik.hadas1@gmail.com>
2019-01-09 15:29:25 +0200

    query vmis running on the node

--- mom/HypervisorInterfaces/kubevirtInterface.py
+++ mom/HypervisorInterfaces/kubevirtInterface.py
@@ -6,13 +6,30 @@ class kubevirtInterface(HypervisorInterface):
 
     def __init__(self, configs):
         config.load_kube_config()
+        self._logger = logging.getLogger('mom.kubevirtInterface')
+        self.v1 = client.CoreV1Api()
 
     def getVmList(self):
-        v1 = client.CoreV1Api()
         print("Listing pods with their IPs:")
-        ret = v1.list_pod_for_all_namespaces(watch=False)
-        for i in ret.items:
-            print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        virt_launcher_label = 'kubevirt.io=virt-launcher'
+        node = 'spec.nodeName=node02'
+        ret = self.v1.list_namespaced_pod('default',
+                                          watch=False,
+                                          label_selector=virt_launcher_label,
+                                          field_selector=node)
+        vmIds = []
+        for pod in ret.items:
+            vmi = get_vmi(pod.metadata.owner_references)
+            vmIds.append(vmi.uid)
+            #print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        self._logger.info('VM List: %s', vmIds)
+        return vmIds
+
+    def get_vmi(owner_references):
+        for owner_reference in owner_references:
+            if owner_reference.controller:
+                return owner_reference
+        return None
 
 def instance(config):
     return kubevirtInterface(config)
